dc4a8d2d62c3ca6ddeb3074cdf22fb2882803f3b
Joey Bruno <brunojoey12@gmail.com>
2021-03-08 13:14:11 -0600

    SocialLogin form udpate.

---
+++ client/src/features/user/SocialLogin.tsx
@@ -0,0 +1,29 @@
+import { observer } from 'mobx-react-lite';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import { Button, Icon } from 'semantic-ui-react';
+
+
+interface IProps {
+  fbCallback: (response: any) => void;
+  loading: boolean;
+};
+
+const SocialLogin: React.FC<IProps> = ({fbCallback, loading}) => {
+  return (
+    <div>
+      <FacebookLogin 
+        appId="754926351797383"
+        fields="name,email,picture"
+        callback={fbCallback}
+        render={(renderProps: any) => (
+          <Button loading={loading} onClick={renderProps.onClick} type='button' fluid color='facebook'>
+            <Icon name="facebook"/>
+            Login with Facebook
+          </Button>
+        )}
+      />
+    </div>
+  )
+}
+
+export default observer(SocialLogin)
