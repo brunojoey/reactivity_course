077b2da05879968f6ba5f946b5e9e908e3e2acf2
Joey Bruno <brunojoey12@gmail.com>
2021-06-22 15:22:22 -0500

    Security fixes.

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,66 +43,88 @@ namespace API
       services.AddDbContext<DataContext>(opt =>
       {
         opt.UseLazyLoadingProxies();
-        opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        opt.UseNpgsql((Configuration.GetConnectionString("DefaultConnection")));
       });
       ConfigureServices(services);
     }
     public void ConfigureProductionServices(IServiceCollection services)
     {
       services.AddDbContext<DataContext>(options =>
-{
-  var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+      {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-  string connStr;
+        string connStr;
 
-  // Depending on if in development or production, use either Heroku-provided
-  // connection string, or development connection string from env var.
-  if (env == "Development")
-  {
-    // Use connection string from file.
-    connStr = Configuration.GetConnectionString("DefaultConnection");
-  }
-  else
-  {
-    // Use connection string provided at runtime by Heroku.
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        // Depending on if in development or production, use either Heroku-provided
+        // connection string, or development connection string from env var.
+        if (env == "Development")
+        {
+          // Use connection string from file.
+          connStr = Configuration.GetConnectionString("DefaultConnection");
+        }
+        else
+        {
+          // Use connection string provided at runtime by Heroku.
+          var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
+          // Parse connection URL to connection string for Npgsql
+          connUrl = connUrl.Replace("postgres://", string.Empty);
+          var pgUserPass = connUrl.Split("@")[0];
+          var pgHostPortDb = connUrl.Split("@")[1];
+          var pgHostPort = pgHostPortDb.Split("/")[0];
+          var pgDb = pgHostPortDb.Split("/")[1];
+          var pgUser = pgUserPass.Split(":")[0];
+          var pgPass = pgUserPass.Split(":")[1];
+          var pgHost = pgHostPort.Split(":")[0];
+          var pgPort = pgHostPort.Split(":")[1];
 
-    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
-  }
+          connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
+        }
 
-  // Whether the connection string came from the local development configuration file
-  // or from the environment variable from Heroku, use it to set up your DbContext.
-  options.UseNpgsql(connStr); 
-});
-
+        // Whether the connection string came from the local development configuration file
+        // or from the environment variable from Heroku, use it to set up your DbContext.
+        options.UseNpgsql(connStr);
+      });
 
       ConfigureServices(services);
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseLazyLoadingProxies();
+        opt.UseNpgsql((Configuration.GetConnectionString("DefaultConnection")));
+      });
+
+      // Allows the API information to get through the CORS block that happens without this. 
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithExposedHeaders("WWW-Authenticate")
+            .WithOrigins("http://localhost:3000")
+            .AllowCredentials();
+        });
+      });
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddAutoMapper(typeof(List.Handler));
+      services.AddSignalR(); // Does not need any additional configurations
+
       // This replaces services.AddMvc in Netcore 3.0
       services.AddControllers(opt =>
       {
         var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
         opt.Filters.Add(new AuthorizeFilter(policy));
       })
-          .AddFluentValidation(cfg =>
-          {
-            // This looks for all validators in the Create class and registers them.
-            cfg.RegisterValidatorsFromAssemblyContaining<Create>();
-          });
+        .AddFluentValidation(cfg =>
+        {
+          // This looks for all validators in the Create class and registers them.
+          cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+        });
 
       // AddIdentityCore because we can add what we need but it will still Identity App User
       var builder = services.AddIdentityCore<AppUser>(options =>
@@ -154,23 +176,6 @@ namespace API
             };
           });
 
-      // Allows the API information to get through the CORS block that happens without this. 
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-        {
-          policy
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .WithExposedHeaders("WWW-Authenticate")
-            .WithOrigins("http://localhost:3000")
-            .AllowCredentials();
-        });
-      });
-      services.AddMediatR(typeof(List.Handler).Assembly);
-      services.AddAutoMapper(typeof(List.Handler));
-      services.AddSignalR(); // Does not need any additional configurations
-
       // Will have access when JWT Tokens are injected
       services.AddScoped<IJwtGenerator, JwtGenerator>();
       // Will get the username out of the token and able to use it anywhere when we inject it
@@ -188,15 +193,18 @@ namespace API
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       app.UseMiddleware<ErrorHandlingMiddleware>();
-
       if (env.IsDevelopment())
       {
         // Commented out to use our middleware
-        // app.UseDeveloperExceptionPage();
+        app.UseDeveloperExceptionPage();
       }
       else
       {
-        // app.UseHsts();
+        app.Use(async (context, next) =>
+        {
+          context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+          await next.Invoke();
+        });
       }
 
       app.UseXContentTypeOptions();
@@ -205,12 +213,12 @@ namespace API
       app.UseXfo(opt => opt.Deny());
       app.UseCsp(opt => opt
         .BlockAllMixedContent()
-        .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-F4GpCPyRepgP5znjMD8sc7PEjzet5Eef4r09dEGPpTs="))
+        .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-4Su6mBWzEIFnH4pAGMOuaeBrstwJN4Z3pq/s1Kn4/KQ=", "sha256-F4GpCPyRepgP5znjMD8sc7PEjzet5Eef4r09dEGPpTs="))
         .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
         .FormActions(s => s.Self())
         .FrameAncestors(s => s.Self())
         .ImageSources(s => s.Self().CustomSources("blob:", "data:"))
-        .ScriptSources(s => s.Self().CustomSources("sha256-vRtWxzPOhvaAr9sSb+lZUZ+2FXHhdPoYb/74kCnyZCo="))
+        .ScriptSources(s => s.Self().CustomSources("sha256-vRtWxzPOhvaAr9sSb+lZUZ+2FXHhdPoYb/74kCnyZCo=", "sha256-5mLg4sgpKNBg8a6QipDJGr/wag/FIA0MKa3Nqy7bp7c="))
       );
 
       // app.UseHttpsRedirection();
